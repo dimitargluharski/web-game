f04e3d6d4c4267f2db1e5d6ce432c8f7e10f104c
Dimitar Gluharski <dimitargluharski@gmail.com>
2025-03-21 20:02:17 +0200

    update dependencies and remove unused files

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,9 @@
-import { Link, Route, Routes } from 'react-router-dom';
-import { TacticsPage } from './pages/TacticsPage/TacticsPage';
-
 function App() {
   return (
-    <div className='flex'>
-      <nav>
-        <Link to="/" />
-        <Link to="/tactics" />
-        <Link to="/team-report" />
-      </nav>
-
-      <main>
-        <Routes>
-          <Route path='/' element={<Link to="/tactics">go to tactics</Link>} />
-          <Route path='/tactics' element={<TacticsPage />} />
-        </Routes>
-      </main>
-    </div>
-  );
+    <>
+      home page
+    </>
+  )
 }
 
-export default App;
+export default App
