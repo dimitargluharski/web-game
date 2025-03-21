5456f4ce0371b4d5f57f0c927c691642c3c022fb
Dimitar Gluharski <dimitargluharski@gmail.com>
2025-03-21 20:07:34 +0200

    init tanstack router

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    TanStackRouterVite({ target: 'react', autoCodeSplitting: true }),
+  ],
   server: {
     open: true
   }
